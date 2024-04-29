bc73358f8b53905de1af0876084d938d59ff08d7
a1kuat <alkuat1511@gmail.com>
2024-04-29 21:35:19 +0100

    Done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,20 @@
 import React from 'react';
-/* import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Login from './components/Login'; // Import your Login component
-import Register from './components/Register'; // Import your Register component */
-import HomePage from './components/Home'; // Import your Home component
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
+import Login from './components/Login'; 
+import Home from './components/Home';
+import Register from './components/Register'; 
 
 function App() {
-
  return (
-    <HomePage />
+    <Router>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/" element={<Navigate to="/login" replace />} />
+      </Routes>
+    </Router>
  );
 }
 
 export default App;
-
---
+++ src/components/Login.js
@@ -0,0 +1,112 @@
+import React, { useState } from 'react';
+import { Box, TextField, Button, Typography, Container, Link, Grid, CssBaseline, Avatar, Snackbar, Alert } from '@mui/material';
+import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+
+const Login = () => {
+ const [username, setUsername] = useState('');
+ const [password, setPassword] = useState('');
+ const [snackbarOpen, setSnackbarOpen] = useState(false);
+
+ const handleLogin = async () => {
+    const response = await fetch('http://localhost:8000/v1/auth/login', {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: `grant_type=&username=${username}&password=${password}&scope=&client_id=&client_secret=`,
+    });
+    console.log(username);
+    console.log(password);
+    if (response.ok) {
+      const data = await response.json();
+      console.log(data);
+     } else {
+      console.error('Login failed');
+      setSnackbarOpen(true); 
+     }
+     
+     
+ };
+
+ const handleSnackbarClose = (event, reason) => {
+    if (reason === 'clickaway') {
+      return;
+    }
+    setSnackbarOpen(false); 
+ };
+
+ return (
+    <Container component="main" maxWidth="xs">
+      <CssBaseline />
+      <Box
+        sx={{
+          marginTop: 8,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+          <LockOutlinedIcon />
+        </Avatar>
+        <Typography component="h1" variant="h5">
+          Sign in
+        </Typography>
+        <Box component="form" onSubmit={handleLogin} sx={{ mt: 1 }}>
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+          />
+          <TextField
+            margin="normal"
+            required
+            fullWidth
+            label="Password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            type="password"
+          />
+          <Button
+            type="submit"
+            fullWidth
+            variant="contained"
+            sx={{ mt: 3, mb: 2 }}
+          >
+            Sign In
+          </Button>
+          <Grid container>
+            <Grid item>
+            <Link href="/register" variant="body2">
+                {"Don't have an account? Sign Up"}
+            </Link>
+            </Grid>
+          </Grid>
+        </Box>
+      </Box>
+      <Snackbar
+        open={snackbarOpen}
+        autoHideDuration={2000}
+        onClose={handleSnackbarClose}
+        anchorOrigin={{ vertical: 'top', horizontal: 'center' }}
+        sx={{
+          width: '100%',
+          "& .MuiSnackbarContent-root": {
+            backgroundColor: 'green',
+            color: 'white',
+          },
+        }}
+      >
+        <Alert onClose={handleSnackbarClose} variant='filled' severity="error" sx={{ width: '80%' }}>
+          Login failed
+        </Alert>
+      </Snackbar>
+    </Container>
+ );
+};
+
+export default Login;
